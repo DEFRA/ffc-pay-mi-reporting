f0048aa1da4428f60325f5305d95cecdf73c2c52
John Watson <johnwatson484@hotmail.com>
2022-04-25 17:02:53 +0100

    Fix storage name

--- ffc-pay-mi-reporting/config.js
+++ ffc-pay-mi-reporting/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   connectionString: process.env.STORAGE_CONNECTION_STRING,
   containerName: 'reports',
-  tableName: 'eventprojection',
+  tableName: 'payeventstore',
   reportName: 'ffc-pay-mi-report.csv'
 }
